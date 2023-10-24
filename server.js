0ac95420f0016fd0afb1584114e44f048eec1c5b
LeVanManh <manh.le@poho.live>
2023-10-24 21:20:20 +0700

    fix cors

--- server.js
+++ server.js
@@ -35,8 +35,21 @@ const uploadImagesFirebaseStorageRouter = require("./api/auth/student/firebase/f
 const updateImageFirebaseStorageRouter = require("./api/auth/teacher/firebase/update_avatar")
 const profileStudentsRouter = require("./api/auth/student/profile")
 const updateAvatarStudentRouter = require("./api/auth/student/firebase/update_avatar")
+const scheduleTeacherRouter = require("./api/home/schedule/schedule")
+
+var whitelist = ['https://backend-shool-project.onrender.com', 'http://localhost:3000']
+var corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (whitelist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true } 
+    } else {
+        corsOptions = { origin: false }
+    }
+    callback(null, corsOptions) 
+}
 
 app.use(cors({ credentials: true, origin: '*' }));
+
 app.use(bodyParser());
 
 app.use('/user', signInRouter)
@@ -69,7 +82,8 @@ app.use('/user', uploadImagesFirebaseStorageRouter)
 app.use('/admin', updateImageFirebaseStorageRouter)
 app.use('/user', profileStudentsRouter)
 app.use('/user', updateAvatarStudentRouter)
+app.use('/admin', scheduleTeacherRouter)
 
-app.listen(port, () => {
+app.listen(port, cors(corsOptionsDelegate), () => {
     console.log(`Server running on port ${port}`);
 });
