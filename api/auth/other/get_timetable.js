18d2b2081e01cc301814b89c2a4af8ec0e82c6ac
Tuyen Ha <tuyen3962@gmail.com>
2023-10-28 22:25:14 +0700

    fix get duration api

--- api/auth/other/get_timetable.js
+++ api/auth/other/get_timetable.js
@@ -79,13 +79,20 @@ router.get('/get-detail/:id', async (req, res) => {
 
 router.get('/get-duration', async (req, res) => {
     try {
+        if(type(req.body.startTime) == "String" || type(req.body.endTime) == "String") {
+            return res.status(205).json({
+                status: "FAIL",
+                message: "Biến đầu vào không hợp lệ",
+                data: result
+            });
+        }
         const classId = req.body.class_id; // mã lớp học
         // thời gian bắt đầu trong tuần
         // ví dụ: bắt đầu là ngày 23/10/2023 thì truyển vào theo time là 23/10/2023 00:00:00
-        const startTime = req.body.startTime;
+        const startTime = new Date(req.body.startTime);
         // thời gian kết thúc trong tuần
         // ví dụ: kết thúc là ngày 23/10/2023 thì truyển vào theo time là 23/10/2023 23:59:59
-        const endTime = req.body.endTime;
+        const endTime = new Date(req.body.endTime);
         const result = await TimeTable.find({
             classId: classId,
             $and: [
