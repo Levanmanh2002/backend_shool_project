ec4c8b69ca2f8d3876526d3d6864f27741700a32
LeVanManh <manh.le@poho.live>
2023-11-05 17:55:05 +0700

    add new class

--- api/home/student/class/class.js
+++ api/home/student/class/class.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 const Class = require('../../../../models/class');
+
+router.post('/create-class', async (req, res) => {
+    try {
+        const className = req.body.className;
+        const classId = uuidv4();
+
+        // Kiểm tra xem lớp học đã tồn tại chưa
+        const existingClass = await Class.findOne({ className });
+
+        if (existingClass) {
+            return res.status(400).json({ error: 'Lớp học đã tồn tại' });
+        }
+
+        // Tạo một lớp học mới
+        const newClass = new Class({ className, classId });
+        await newClass.save();
+
+        res.status(201).json({
+            message: 'Lớp học mới đã được tạo',
+            class: newClass,
+            id: newClass.classId,
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Lỗi máy chủ nội bộ' });
+    }
+});
 
 router.get('/classes', async (req, res) => {
     try {
         const classes = await Class.find();
 
         res.status(201).json({
+            status: "SUCCESS",
             totalClasses: classes.length,
-            classes: classes.map(cls => ({ className: cls.className, id: cls.id })),
+            classes: classes.map(cls => ({ className: cls.className, id: cls.id, classId: cls.classId })),
         });
     } catch (error) {
         console.error('Error:', error);
