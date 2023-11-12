addf6e0abc7ac466a50db6623d7d674869bb9264
Tuyen Ha <tuyen3962@gmail.com>
2023-11-12 18:36:11 +0700

    add new chart for week

--- api/home/chart/chart.js
+++ api/home/chart/chart.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const datetime = require('date-and-time');
 
 const Student = require('../../../models/student');
 
@@ -26,6 +27,47 @@ router.get('/countNewStudents', async (req, res) => {
                 count,
                 students,
             }
+        });
+    } catch (error) {
+        console.error('Lỗi:', error);
+        res.status(500).json({ error: 'Đã có lỗi xảy ra' });
+    }
+});
+
+router.get('/countNewStudentInWeek', async (req, res) => {
+    const { startDate } = req.query;
+
+    try {
+        if (startDate == undefined) {
+            return res.status(400).json({ error: 'Ngày không hợp lệ' });
+        }
+
+        const start = new Date(parseInt(startDate));
+        console.log(start);
+
+        const endDate =  datetime.addDays(start, 7);
+
+        const students = await Student.find({
+            createdAt: {
+                $gte: start,
+                $lte: endDate,
+            },
+        });
+
+        const result = [];
+        for(var i = 0; i < 7; i++) {
+            const currentDay = datetime.addDays(start, i);
+            const currentStudent = students.filter((val ,index, arr) => val.createdAt.getDate() == currentDay.getDate() && val.createdAt.getMonth() == currentDay.getMonth());
+            result.push({
+                nunber: currentStudent.length,
+                student: currentStudent,
+                day: currentDay.getDate() + '/' + currentDay.getMonth() + '/' + currentDay.getFullYear(),
+            });
+        }
+
+        res.status(201).json({
+            status: 'SUCCESS',
+            data: result
         });
     } catch (error) {
         console.error('Lỗi:', error);
