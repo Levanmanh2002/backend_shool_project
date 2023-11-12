64b84abbba62ae3ae626c1f94a8c5d1a9222c7aa
LeVanManh <manh.le@poho.live>
2023-11-12 14:05:57 +0700

    fix chart

--- api/home/chart/chart.js
+++ api/home/chart/chart.js
@@ -7,16 +7,21 @@ router.get('/countNewStudents', async (req, res) => {
     const { startDate, endDate } = req.query;
 
     try {
-        const count = await Student.countDocuments({
+        const students = await Student.find({
             createdAt: {
                 $gte: new Date(startDate),
                 $lte: new Date(endDate),
             },
         });
 
+        const count = students.length;
+
         res.status(201).json({
             status: 'SUCCESS',
-            data: count
+            data: {
+                count,
+                students,
+            }
         });
     } catch (error) {
         console.error('Lỗi:', error);
