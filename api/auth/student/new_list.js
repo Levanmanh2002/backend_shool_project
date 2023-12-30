9cf9d5d9b60f462566050036f3bc9cfe67d9e0c8
LeVanManh <manh.le@poho.live>
2023-12-31 03:50:39 +0700

    page new list

--- api/auth/student/new_list.js
+++ api/auth/student/new_list.js
@@ -2,6 +2,30 @@ const express = require('express');
 const router = express.Router();
 const Student = require('../../../models/student');
 
+router.get('/total_new_list', async (req, res) => {
+    try {
+        const currentYear = new Date().getFullYear();
+
+        const firstDayOfCurrentYear = new Date(currentYear, 0, 1);
+        const firstDayOfNextYear = new Date(currentYear + 1, 0, 1);
+
+        const studentsEnrolledThisYear = await Student.find({
+            createdAt: { $gte: firstDayOfCurrentYear, $lt: firstDayOfNextYear },
+        });
+
+        res.status(201).json({
+            status: "SUCCESS",
+            message: 'Danh sách học sinh mới',
+            total: studentsEnrolledThisYear.length,
+            students: studentsEnrolledThisYear,
+        });
+    } catch (error) {
+        console.error('Lỗi khi lấy danh sách học sinh:', error);
+        res.status(500).json({ error: 'Lỗi khi xử lý yêu cầu' });
+    }
+});
+
+
 router.get('/new_list', async (req, res) => {
     try {
         const currentYear = new Date().getFullYear();
@@ -29,6 +53,5 @@ router.get('/new_list', async (req, res) => {
         res.status(500).json({ error: 'Lỗi khi xử lý yêu cầu' });
     }
 });
-
 
 module.exports = router;
