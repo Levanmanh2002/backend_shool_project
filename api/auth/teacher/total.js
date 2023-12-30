c6f6b87dd6e0791d4734e3dd86e23da6ebcc48e2
LeVanManh <manh.le@poho.live>
2023-12-31 03:58:19 +0700

    add page total limit teacher

--- api/auth/teacher/total.js
+++ api/auth/teacher/total.js
@@ -17,4 +17,26 @@ router.get('/total', async (req, res) => {
     }
 });
 
+router.get('/total_page', async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+
+        const totalTeachers = await Teacher.find({})
+            .skip((page - 1) * limit)
+            .limit(limit);
+
+        res.status(201).json({
+            status: "SUCCESS",
+            total: totalTeachers.length,
+            message: "Danh sách các giáo viên",
+            data: totalTeachers
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Lỗi máy chủ nội bộ' });
+    }
+});
+
+
 module.exports = router;
