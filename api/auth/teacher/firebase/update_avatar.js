fb2d7bb7ec35eeba16fd6883515c08bf2e88b746
LeVanManh <manh.le@poho.live>
2024-03-03 20:22:36 +0700

    upload avatar teacher

--- api/auth/teacher/firebase/update_avatar.js
+++ api/auth/teacher/firebase/update_avatar.js
@@ -19,30 +19,17 @@ const upload = multer({
     storage: multer.memoryStorage()
 })
 
-router.post('/update-avatar', upload.single('file'), async (req, res) => {
+router.post('/upload-avatar-teacher', upload.single('file'), async (req, res) => {
     try {
         const { teacherId } = req.body;
         const file = req.file;
 
-        if (!file) {
-            return res.status(400).json({ error: 'Không có tệp hình ảnh.' });
-        }
-
-        if (!teacherId || teacherId.length !== 24) {
-            return res.status(400).json({ error: 'teacherId không hợp lệ.' });
-        }
-
-        const teacherIdCheck = await Teacher.findById(teacherId);
-        if (!teacherIdCheck) {
-            return res.status(400).json({ error: 'Giáo viên không tồn tại.' });
-        }
-
-        const tempFilePath = `temp_${teacherId}_${file.originalname}`;
+        const tempFilePath = `teacher_avatars_${teacherId}_${file.originalname}.jpg`;
         fs.writeFileSync(tempFilePath, file.buffer);
 
         const filePath = tempFilePath
         const fileName = `${teacherId}-${file.originalname}`;
-        const fileDestination = `graduation-certificates/teacher_image/${teacherId}/${fileName}`;
+        const fileDestination = `teacher_avatar/${teacherId}/${fileName}`;
 
         await bucket.upload(filePath, {
             destination: fileDestination,
@@ -50,108 +37,116 @@ router.post('/update-avatar', upload.single('file'), async (req, res) => {
 
         fs.unlinkSync(tempFilePath);
 
-        const [url] = await bucket.file(fileDestination).getSignedUrl({
+        const imageUrl = await bucket.file(fileDestination).getSignedUrl({
             action: 'read',
             expires: '03-01-2500',
         });
 
-        teacherId.avatarUrl = url;
+        // let teacher = await Teacher.findOne({ teacherId });
 
-        res.status(201).json({ message: 'Tải ảnh lên Firebase thành công.', avatarUrl: url });
+        // if (teacher.avatarUrl) {
+        //     teacher.avatarUrl = imageUrl[0];
+        // } else {
+        //     teacher.avatarUrl = imageUrl[0];
+        // }
+
+        // await teacher.save();
+
+        res.status(201).json({ message: 'Avatar uploaded and URL saved successfully.' });
     } catch (error) {
-        console.error('Lỗi khi tải ảnh lên Firebase:', error);
-        res.status(500).json({ error: 'Lỗi khi tải ảnh lên Firebase.' });
+        console.error('Error uploading avatar:', error);
+        res.status(500).json({ error: 'An error occurred while uploading avatar.' });
     }
 });
 
-router.put('/edit-avatar/:teacherId', upload.single('file'), async (req, res) => {
-    try {
-        const teacherId = req.params.teacherId;
-        const file = req.file;
+// router.put('/edit-avatar/:teacherId', upload.single('file'), async (req, res) => {
+//     try {
+//         const teacherId = req.params.teacherId;
+//         const file = req.file;
 
-        if (!file) {
-            return res.status(400).json({ error: 'Không có tệp hình ảnh.' });
-        }
+//         if (!file) {
+//             return res.status(400).json({ error: 'Không có tệp hình ảnh.' });
+//         }
 
-        if (!teacherId || teacherId.length !== 24) {
-            return res.status(400).json({ error: 'teacherId không hợp lệ.' });
-        }
+//         if (!teacherId || teacherId.length !== 24) {
+//             return res.status(400).json({ error: 'teacherId không hợp lệ.' });
+//         }
 
-        const teacher = await Teacher.findById(teacherId);
-        if (!teacher) {
-            return res.status(404).json({ error: 'Giáo viên không tồn tại.' });
-        }
+//         const teacher = await Teacher.findById(teacherId);
+//         if (!teacher) {
+//             return res.status(404).json({ error: 'Giáo viên không tồn tại.' });
+//         }
 
-        const tempFilePath = `temp_${teacherId}_${file.originalname}`;
-        fs.writeFileSync(tempFilePath, file.buffer);
-        const filePath = tempFilePath;
-        const fileName = `${teacherId}-${file.originalname}`;
-        const fileDestination = `graduation-certificates/teacher_image/${teacherId}/${fileName}`;
-        await bucket.upload(filePath, {
-            destination: fileDestination,
-        });
-        fs.unlinkSync(tempFilePath);
+//         const tempFilePath = `temp_${teacherId}_${file.originalname}`;
+//         fs.writeFileSync(tempFilePath, file.buffer);
+//         const filePath = tempFilePath;
+//         const fileName = `${teacherId}-${file.originalname}`;
+//         const fileDestination = `graduation-certificates/teacher_image/${teacherId}/${fileName}`;
+//         await bucket.upload(filePath, {
+//             destination: fileDestination,
+//         });
+//         fs.unlinkSync(tempFilePath);
 
-        const [url] = await bucket.file(fileDestination).getSignedUrl({
-            action: 'read',
-            expires: '03-01-2500',
-        });
+//         const [url] = await bucket.file(fileDestination).getSignedUrl({
+//             action: 'read',
+//             expires: '03-01-2500',
+//         });
 
-        teacher.avatarUrl = url;
-        await teacher.save();
+//         teacher.avatarUrl = url;
+//         await teacher.save();
 
-        res.status(201).json({ message: 'Chỉnh sửa ảnh thành công.', newAvatarUrl: url });
-    } catch (error) {
-        console.error('Lỗi khi chỉnh sửa ảnh:', error);
-        res.status(500).json({ error: 'Lỗi khi chỉnh sửa ảnh.' });
-    }
-});
+//         res.status(201).json({ message: 'Chỉnh sửa ảnh thành công.', newAvatarUrl: url });
+//     } catch (error) {
+//         console.error('Lỗi khi chỉnh sửa ảnh:', error);
+//         res.status(500).json({ error: 'Lỗi khi chỉnh sửa ảnh.' });
+//     }
+// });
 
 
-router.get('/images/:teacherId', async (req, res) => {
-    try {
-        const teacherId = req.params.teacherId;
-        const folderPath = `graduation-certificates/teacher_image/${teacherId}/`;
-        const [files] = await bucket.getFiles({ prefix: folderPath });
+// router.get('/images/:teacherId', async (req, res) => {
+//     try {
+//         const teacherId = req.params.teacherId;
+//         const folderPath = `graduation-certificates/teacher_image/${teacherId}/`;
+//         const [files] = await bucket.getFiles({ prefix: folderPath });
 
-        const imageUrls = [];
+//         const imageUrls = [];
 
-        for (const file of files) {
-            const imageUrl = await file.getSignedUrl({
-                action: 'read',
-                expires: '03-01-2500',
-            });
-            imageUrls.push(imageUrl);
-        }
+//         for (const file of files) {
+//             const imageUrl = await file.getSignedUrl({
+//                 action: 'read',
+//                 expires: '03-01-2500',
+//             });
+//             imageUrls.push(imageUrl);
+//         }
 
-        res.status(201).json({
-            status: "SUCCESS",
-            message: "Avatar của " + teacherId,
-            data: imageUrls,
-        });
-    } catch (error) {
-        console.error('Lỗi khi tải ảnh:', error);
-        res.status(500).json({ error: 'Lỗi khi tải ảnh.' });
-    }
-});
+//         res.status(201).json({
+//             status: "SUCCESS",
+//             message: "Avatar của " + teacherId,
+//             data: imageUrls,
+//         });
+//     } catch (error) {
+//         console.error('Lỗi khi tải ảnh:', error);
+//         res.status(500).json({ error: 'Lỗi khi tải ảnh.' });
+//     }
+// });
 
-router.delete('/delete-images/:teacherId', async (req, res) => {
-    try {
-        const { teacherId } = req.params;
+// router.delete('/delete-images/:teacherId', async (req, res) => {
+//     try {
+//         const { teacherId } = req.params;
 
-        const teacherFolder = `graduation-certificates/teacher_image/${teacherId}`;
+//         const teacherFolder = `graduation-certificates/teacher_image/${teacherId}`;
 
-        const [files] = await bucket.getFiles({ prefix: teacherFolder });
+//         const [files] = await bucket.getFiles({ prefix: teacherFolder });
 
-        await Promise.all(files.map(async (file) => {
-            await file.delete();
-        }));
+//         await Promise.all(files.map(async (file) => {
+//             await file.delete();
+//         }));
 
-        res.status(201).json({ message: `Đã xóa tất cả hình ảnh trong thư mục của teacherId ${teacherId}.` });
-    } catch (error) {
-        console.error('Lỗi khi xóa hình ảnh:', error);
-        res.status(500).json({ error: 'Lỗi khi xóa hình ảnh.' });
-    }
-});
+//         res.status(201).json({ message: `Đã xóa tất cả hình ảnh trong thư mục của teacherId ${teacherId}.` });
+//     } catch (error) {
+//         console.error('Lỗi khi xóa hình ảnh:', error);
+//         res.status(500).json({ error: 'Lỗi khi xóa hình ảnh.' });
+//     }
+// });
 
 module.exports = router;
